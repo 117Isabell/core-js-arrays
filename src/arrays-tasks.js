a0e7464780b758d2cae3497f2b7260fab93e4a10
117Isabell <isabellhepburn@outlook.com>
2024-12-26 18:02:50 +0100

    feat: add solutions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,13 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+  // const arr = [];
+  // for (let i = start; i <= end; i += 1) {
+  //   arr.push(i);
+  // }
+  // return arr;
 }
 
 /**
@@ -37,8 +42,12 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const maxLength = Math.max(arr1.length, arr2.length);
+
+  const paddedArr1 = [...arr1, ...Array(maxLength - arr1.length).fill(0)];
+  const paddedArr2 = [...arr2, ...Array(maxLength - arr2.length).fill(0)];
+  return paddedArr1.map((el, i) => el + paddedArr2[i]);
 }
 
 /**
